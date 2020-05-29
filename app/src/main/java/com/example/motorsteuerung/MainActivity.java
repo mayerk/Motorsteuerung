50c6f066f3609203216514357c1aee484e8b9f9d
mayerk <kilianmayer@gmx.at>
2020-05-29 15:07:19 +0200

    Moin JUngs

--- app/src/main/java/com/example/motorsteuerung/MainActivity.java
+++ app/src/main/java/com/example/motorsteuerung/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.motorsteuerung;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -21,10 +30,14 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.io.UnsupportedEncodingException;
 import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button connect, publish;
+    private RelativeLayout dirRight, dirLeft;
+
+    private Spinner speedDropdown;
 
     String serverURL = "tcp://farmer.cloudmqtt.com:13524";
     String username = "fnxrzwib";
@@ -33,16 +46,25 @@ public class MainActivity extends AppCompatActivity {
     String clientID = MqttClient.generateClientId();
     MqttAndroidClient client;
 
+    String dirMessage, enableMessage, motionSpeed;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dirRight = findViewById(R.id.directionRight);
+        dirLeft = findViewById(R.id.directionLeft);
+
+        speedDropdown = findViewById(R.id.speedDropdown);
 
         client = new MqttAndroidClient(MainActivity.this, serverURL, clientID);
 
         final MqttConnectOptions options = new MqttConnectOptions();
         options.setUserName(username);
         options.setPassword(password.toCharArray());
+
 
         if (!client.isConnected()) {
             try {
@@ -72,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    Toast.makeText(MainActivity.this, "ESP ist connected", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "ESP is connected", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
@@ -84,35 +106,73 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(MainActivity.this, "already connected", Toast.LENGTH_SHORT).show();
         }
-    }
 
-    public void publish(String topic) {
-        String message = "test";
-        try {
-            client.publish(topic, message.getBytes(), 0, false);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
+        dirLeft.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        dirMessage = "CCW";
+                        enableMessage = "on";
+                        motionSpeed = speedDropdown.getSelectedItem().toString();
+                        if (client.isConnected()) {
+                            try {
+                                client.publish("clock", String.valueOf(motionSpeed).getBytes(), 0, false);
+                                client.publish("enable", enableMessage.getBytes(), 0, false);
+                                client.publish("direction", dirMessage.getBytes(), 0, false);
+                            } catch (MqttException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return true;
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        try {
-            IMqttToken disconToken = client.disconnect();
-            disconToken.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
+                    case MotionEvent.ACTION_UP:
+                        enableMessage = "off";
+                        if (client.isConnected()) {
+                            try {
+                                client.publish("enable", enableMessage.getBytes(), 0, false);
+                            } catch (MqttException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return true;
                 }
+                return false;
+            }
+        });
 
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+        dirRight.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        dirMessage = "CW";
+                        enableMessage = "on";
+                        motionSpeed = speedDropdown.getSelectedItem().toString();
+                        if (client.isConnected()) {
+                            try {
+                                client.publish("clock", String.valueOf(motionSpeed).getBytes(), 0, false);
+                                client.publish("enable", enableMessage.getBytes(), 0, false);
+                                client.publish("direction", dirMessage.getBytes(), 0, false);
+                            } catch (MqttException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        enableMessage = "off";
+                        if (client.isConnected()) {
+                            try {
+                                client.publish("enable", enableMessage.getBytes(), 0, false);
+                            } catch (MqttException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return true;
                 }
-            });
-            
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
+                return false;
+            }
+        });
     }
 }
